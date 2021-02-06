29b227cbaf64b70351e31c8d6ffc708681f39609
Makoto <makoto@emurasoft.com>
2021-02-06 10:24:26 -0800

    Fix nestedloop.s

--- Part1/nestedloop.s
+++ Part1/nestedloop.s
@@ -9,14 +9,17 @@ addr_j: .word j
 addr_x: .word x
 
 main:
-	mov r0, [addr_i]	// i = 1
-	mov r1, [addr_j]	// j = 0
-	mov r2, [addr_x]	// x = 0
+	ldr r0, addr_i
+	ldr r0, [r0]			// i = 1
+	ldr r1, addr_j
+	ldr r1, [r1]			// j = 0
+	ldr r2, addr_x
+	ldr r2, [r2]			// x = 0
 
 	cmp r0, #10
-	bge exit		// i >= 10
+	bge exit			// i >= 10
 outer:
-	mov r1, r0		// j = i
+	mov r1, r0			// j = i
 	cmp r1, #10
 	bge outer_end		// j >= 10
 inner:
@@ -24,11 +27,11 @@ inner:
 	add r2, r2, r3		// x = x + tmp
 	add r1, r1, #1		// j++
 	cmp r1, #10
-	blt inner		// j < 10
+	blt inner			// j < 10
 outer_end:
 	add r0, r0, #2		// i += 2
 	cmp r0, #10
-	blt outer		// i < 10
+	blt outer			// i < 10
 
 exit:
 	mov r0, r2
